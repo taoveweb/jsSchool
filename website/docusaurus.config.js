a9c95e29616a775a1f551134b3f8549c79ad2cd9
matt <167104@qq.com>
2020-09-16 10:19:07 +0800

    all

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,8 +8,8 @@
 const siteConfig = {
   title: "前端-学习笔记",
   tagline: "笔记",
-  url: "http://shuawei.com/doc",
-  baseUrl: "/",
+  url: "http://oldku.com",
+  baseUrl: "/doc/",
   projectName: "前端-学习笔记",
   organizationName: "jlt",
   favicon: "img/favicon/favicon.ico",
